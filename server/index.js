8d5459cf25063f0c854de57863d2ab164c19208d
Denis Tsoi <denistsoi@gmail.com>
2020-03-11 15:39:53 +0800

    ADD: bump version to 0.3.0; improve readability;

--- server/index.js
+++ server/index.js
@@ -1,20 +1,21 @@
 const http = require("http")
 const WebSocket = require("ws");
 const wss = new WebSocket.Server({ port: 3001 })
-const Game = require("./Game")
 
+// common lib
+const Game = require("./Game")
 const RandomColor = require("./color/RandomColor");
 
 // generate random id + color
 const { height, width, timePerLife } = require("./config")
+const game = new Game({ height, width });
 
+// helper fn
 const stringify = object => JSON.stringify(object)
 
-const game = new Game({ height, width });
 
 wss.on("connection", (ws, req, client) => {
   // on connection, re-create the app state (i.e game state)
-
   ws.send(
     stringify({
       type: "connect",
